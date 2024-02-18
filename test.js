866846c4368d9a3c78c72a4d19683acb63faa6ce
Alexander-1092 <Alex-Sadr1092@yandex.ru>
2024-02-18 17:04:36 +0300

    feat: add text

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 function XO(str) {
-  const xo = str.toLocaleLowerCase().split('')
+	const xo = str.toLocaleLowerCase().split('')
 	const x = xo.filter(element => element == 'x')
 	const o = xo.filter(element => element == 'o')
-	if (x.length == o.length || (!xo.includes('o') && !xo.includes('x'))) 
-	{return true} else {return false}
+	if (x.length == o.length || (!xo.includes('o') && !xo.includes('x'))) { return true } else { return false }
 }
+console.log('hi');
